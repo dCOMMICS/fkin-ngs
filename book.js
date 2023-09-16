053fea32daa7d13db23e95c6f59e75bdcf29ae6c
dCOMMICS <sanwurld@gmail.com>
2023-09-17 01:21:03 +0300

    arrays and objects

---
+++ book.js
@@ -0,0 +1,9 @@
+// Arrays and objects can hold other arrays and objects:
+var points = [ // An array with 2 elements.
+ {x:0, y:0}, // Each element is an object.
+ {x:1, y:1}
+];
+var data = { // An object with 2 properties
+ trial1: [[1,2], [3,4]], // The value of each property is an array.
+ trial2: [[2,3], [4,5]] // The elements of the arrays are arrays.
+};
