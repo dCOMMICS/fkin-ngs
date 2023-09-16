3e2718bd228103667f4465b19e341a4105e73a68
dCOMMICS <sanwurld@gmail.com>
2023-09-17 01:50:38 +0300

    frgs = new

--- unihomework.cs
+++ unihomework.cs
@@ -1,6 +1,6 @@
 class HelloCSharp
 {
-static void Main(string[] args)
+static void Main(string[] args) // starting point for our application //
 {
 System.Console.WriteLine("Hello C#!"); // should print Hello C# // 
 
